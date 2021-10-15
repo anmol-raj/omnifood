b9ca28a90fec2c70708f21dc7764c0978445a427
Anmol Raj <61791716+anmol-raj@users.noreply.github.com>
2021-10-15 18:53:02 +0530

    Omni food project

---
+++ Omnifood/js/script.js
@@ -0,0 +1,57 @@
+// console.log("hello omni...");
+// const myName = "Anmol Raj";
+// const h1 = document.querySelector(".heading-primary");
+// console.log(myName);
+// console.log(h1);
+
+// h1.addEventListener("click", function () {
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
+
+///////////////////////////////////////////
+// Set currentyear
+
+const yearEl = document.querySelector(".year");
+const currentyear = new Date().getFullYear();
+console.log(currentyear);
+yearEl.textContent = currentyear;
+
+///////////////////////////////////////////
+// Make Mobile Navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////
+// Smooth scroll
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
